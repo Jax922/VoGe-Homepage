ffca356cce4e28f9b86c9e0d89d4f02683b9d734
Dong She <dshe922@gmail.com>
2024-02-13 16:18:06 +0100

    init

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,23 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/", { loader: () => import(/* webpackChunkName: "v-4ec95bbd" */"/Users/shedong/Documents/dev/VoGe-Homepage/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Hello VuePress"} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "v-8fe89ed2" */"/Users/shedong/Documents/dev/VoGe-Homepage/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
