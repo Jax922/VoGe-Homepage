609ad32dde1c3d58af90b065033ddd6a4519ccc6
Dong She <dshe922@gmail.com>
2024-02-13 17:16:01 +0100

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,14 +5,17 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     base: "/VoGe-Homepage/",
     bundler: viteBundler(),
-    theme: defaultTheme(),
+    theme: defaultTheme({
+        navbar: [
+            { text: 'Home', link: '/' },
+            { text: 'VoGe-Portal', link: 'https://github.com/Jax922/VoGe-Portal' },
+            { text: 'VoGe-Display', link: 'https://github.com/Jax922/VoGe-Display' },
+            { text: 'VoGe-NLU', link: 'https://github.com/Jax922/VoGe_NLU'}
+        ],
+    }),
     lang: 'en-US',
-    title: "VoGe Presentation",
+    title: "VoGe Presentation Docs",
 //   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
-    navbar: [
-        { text: 'Home', link: '/' },
-        { text: 'VoGe-Portal', link: 'https://github.com/Jax922/VoGe-Portal' },
-        { text: 'VoGe-Display', link: 'https://github.com/Jax922/VoGe-Display' },
-        { text: 'VoGe-NLU', link: 'https://github.com/Jax922/VoGe_NLU'}
-    ]
+    
+
 })
